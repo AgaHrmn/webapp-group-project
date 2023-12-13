e8442745b3ad16b58bdce81975eed22304f91c6f
agaolejniq <a.olejnikk@gmail.com>
2023-12-13 01:45:03 +0100

    szkielet projektu

---
+++ saper-app/saper.js
@@ -0,0 +1,24 @@
+// variables / zmienne 
+
+// function to add board / dodanie planszy (10x10)
+
+    //create array of bombs randomly distributed on the board
+    //stwórz listę bomb i zrozmieść je losowo na planszy
+
+    //create event on each square 
+    //dodaj event do każdego pola 
+
+    //numbers  
+    //numerki 
+
+//function to add flags on board witch a right click
+//rozmieść flagi prawym przyciskiem myszy
+
+//function to check if bomb was clicked
+//sprawdz czy kliknięto bombę
+
+//function to check neighboring bombs
+//sprawdz sąsiadujące pola (czy mają bomby)
+
+//function to check if user has won
+//sprawdz czy wygrana 
