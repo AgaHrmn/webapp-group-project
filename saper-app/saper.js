0f2764e9e1a82f5ed3074bc3f1d4d3745d8f05e1
agaolejniq <a.olejnikk@gmail.com>
2024-01-23 17:33:26 +0100

    added levels, each level adds 2 bombs -----to implement: changing board dimensions

--- saper-app/saper.js
+++ saper-app/saper.js
@@ -1,13 +1,19 @@
 // variables / zmienne
-const level = 1;
-const board = [];
-const rows = 8;
-const columns = 8;
-const bombs_left = 2;
+
+const levelsList = [
+    { rows: 8, columns: 8, bombs: 5 },
+    { rows: 8, columns: 8, bombs: 7 },
+    { rows: 8, columns: 8, bombs: 9 }
+];
+let level = 0;
+let nextLvlButtonGenerated = false;
+let board = [];
 const bombs_location = [];
 let squares_clicked = 0;
 const is_flagged = false;
 let game_over_flag = false;
+let lvl_won_flag = false;
+
 
 window.onload = function () {
     startGame();
@@ -15,12 +21,12 @@ window.onload = function () {
 
 // function to add board / dodanie planszy
 function startGame() {
-    document.getElementById("bombs_left").innerText = "(☞ ͡° ͜ʖ ͡°)☞" + bombs_left + "💣";
+    document.getElementById("bombs_left").innerText = "( ͡° ͜ʖ ͡°)☞" + levelsList[level].bombs + "💣";
     generateCoordinates();
 
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < levelsList[level].rows; i++) {
         let row = [];
-        for (let v = 0; v < columns; v++) {
+        for (let v = 0; v < levelsList[level].columns; v++) {
             let square = document.createElement("div");
             square.id = i.toString() + "-" + v.toString();
             square.addEventListener("click", clickSquare);
@@ -31,7 +37,6 @@ function startGame() {
         board.push(row);
     }
 }
-
 
 // function playExplosionSound() {
 //     const explosionSound = document.getElementById("explosionSound");
@@ -62,9 +67,7 @@ function clickSquare() {
     if (game_over_flag || this.classList.contains("square_clicked")) {
         return;
     }
-
     let square = this;
-
     if (is_flagged) {
         if (square.innerText == "") {
             square.innerText = "🚩";
@@ -76,7 +79,17 @@ function clickSquare() {
     if (gameOver(square)) {
         playAgain();
         return;
-    };
+    } else if (lvlWon(square)) {
+        if (gameWon()) {
+            let nextLvl_butt = document.getElementById("nextLvl_butt");
+            nextLvl_butt.removeEventListener("click", lvlUp);
+            nextLvl_butt.textContent = "( ͡° ͜ʖ ͡°)"
+            document.getElementById("bombs_left").innerText = "Congratulations! You've won the game!";
+        } else {
+            nextLvl();
+        }
+        return;
+    }
 
     let squareCoordinates = square.id.split("-");
     let r = parseInt(squareCoordinates[0]);
@@ -87,21 +100,21 @@ function clickSquare() {
 // function to generate random coordinates / wygeneruj koordynanty dla bomb
 function generateCoordinates() {
     do {
-        let r = Math.floor(Math.random() * bombs_left);
-        let c = Math.floor(Math.random() * bombs_left);
+        let r = Math.floor(Math.random() * levelsList[level].bombs);
+        let c = Math.floor(Math.random() * levelsList[level].bombs);
         let coordinate = r.toString() + "-" + c.toString();
         if (!bombs_location.includes(coordinate)) {
             bombs_location.push(coordinate)
         }
-    } while (bombs_location.length < bombs_left);
+    } while (bombs_location.length < levelsList[level].bombs);
     squares_clicked += 1;
     console.log(bombs_location);
 }
 
 // function to show all the bombs / pokaż wszystkie bomby na planszy
 function showBombs() {
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < levelsList[level].rows; r++) {
+        for (let c = 0; c < levelsList[level].columns; c++) {
             let square = board[r][c];
             if (bombs_location.includes(square.id)) {
                 square.innerText = "💣";
@@ -113,7 +126,7 @@ function showBombs() {
 
 // function to show neighboring bombs / sprawdz sąsiadujące pola (czy mają bomby)
 function showNeighboringBombs(r, c) {
-    if (r < 0 || r >= rows || c < 0 || c >= columns) {
+    if (r < 0 || r >= levelsList[level].rows || c < 0 || c >= levelsList[level].columns) {
         return;
     }
     if (board[r][c].classList.contains("square_clicked")) {
@@ -146,7 +159,7 @@ function showNeighboringBombs(r, c) {
 
 // function to check neighboring squares / sprawdz sąsiadujące pola (czy mają bomby)
 function checkSquare(r, c) {
-    if (r < 0 || r >= rows || c < 0 || c >= columns) {
+    if (r < 0 || r >= levelsList[level].rows || c < 0 || c >= levelsList[level].columns) {
         return 0;
     }
     if (bombs_location.includes(r.toString() + "-" + c.toString())) {
@@ -163,17 +176,28 @@ function gameOver(s) {
         game_over_flag = true;
         return true;
     }
-    if (squares_clicked == rows * columns - bombs_left) {
-        document.getElementById("bombs_left").innerText = "(੭˃ᴗ˂)੭";
-        if (!bombs_location.includes(s.id)) {
-            s.style.backgroundColor = "lightslategray"
-        }
-        game_over_flag = true;
-        return true;
-    }
-    game_over_flag = false;
     return false;
 }
+
+function clearBoard() {
+    let boardContainer = document.getElementById("board");
+
+    boardContainer.childNodes.forEach(row => {
+        row.childNodes.forEach(square => {
+            square.removeEventListener("click", clickSquare);
+            square.removeEventListener("contextmenu", putFlag);
+        });
+    });
+
+    while (boardContainer.firstChild) {
+        boardContainer.removeChild(boardContainer.firstChild);
+    }
+
+    board.length = 0;
+    bombs_location.length = 0;
+    squares_clicked = 0;
+}
+
 
 function resetGame() {
     location.reload();
@@ -185,10 +209,54 @@ function playAgain() {
     play_again_butt.textContent = "Jeszcze raz?";
 
     let firstChild = document.body.firstChild;
-
-    // Insert the button and line break before the first child
     document.body.insertBefore(brake_line, firstChild);
     document.body.insertBefore(play_again_butt, firstChild);
 
     play_again_butt.addEventListener("click", resetGame);
 }
+
+function nextLvl() {
+    if (!nextLvlButtonGenerated) {
+        let brake_line = document.createElement("br")
+        let nextLvl_butt = document.createElement("button");
+        nextLvl_butt.textContent = "Kolejny level?";
+        nextLvl_butt.id = "nextLvl_butt";
+
+        let firstChild = document.body.firstChild;
+        document.body.insertBefore(brake_line, firstChild);
+        document.body.insertBefore(nextLvl_butt, firstChild);
+
+        nextLvl_butt.addEventListener("click", lvlUp);
+        nextLvlButtonGenerated = true;
+    }
+}
+
+function lvlUp() {
+    if (level < levelsList.length) {
+        level++;
+        game_over_flag = false;
+        lvl_won_flag = false;
+        clearBoard();
+        startGame();
+    }
+}
+
+function lvlWon(s) {
+    if (squares_clicked == levelsList[level].rows * levelsList[level].columns - levelsList[level].bombs) {
+        document.getElementById("bombs_left").innerText = "Udało ci się skończyć level " + level;
+        if (!bombs_location.includes(s.id)) {
+            s.style.backgroundColor = "lightslategray"
+        }
+        lvl_won_flag = true;
+        return true;
+    }
+    return false;
+}
+
+function gameWon() {
+    if (level === levelsList.length-1) {
+        return true;
+    }
+    return false;
+}
+
